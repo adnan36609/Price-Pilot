8f6f3dca3cdd64f880314cb93fb83ed7b91d72e3
adnan36609 <adnan36609@gmail.com>
2026-08-22 20:16:58 +0530

    createClient import modified

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/lib/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
